f525f5f792949b54c9d2c31a9e6d49164e3ae3e1
rajesh <rajeshwaran.b@gmail.com>
2026-03-03 16:31:12 -0600

    updated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="RajaRajeshwaran",
+    author_email="rajeshwaran.actai@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
